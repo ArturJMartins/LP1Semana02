689db922db657603927ba10779da0be0e3b030f6
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-28 23:51:37 +0000

    Incrementar e decrementar números inteiros

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não-negativo: ");
+            string usuario = Console.ReadLine();
+
+            byte n = byte.Parse(usuario);
+            
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
