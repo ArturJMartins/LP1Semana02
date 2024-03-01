e632f5448039cec1e2814ecad7347b8f2ce05c5d
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-01 13:20:33 +0000

    Volume e area de superficie do cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a altura do cilindro: ");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o raio do cilindro: ");
+            float r = float.Parse(Console.ReadLine());
+
+            float volume = MathF.PI * MathF.Pow(r,2) * a;
+            float areaDaSuperficie = 2 * MathF.PI * r * (r + a);
+
+            Console.WriteLine($"O volume do cilindro é {volume}.");
+            Console.WriteLine("A área de superfície do cilindro é " + 
+            $"{areaDaSuperficie}.");
+        }
+    }
+}
