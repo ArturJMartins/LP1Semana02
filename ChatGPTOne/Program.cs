9b35d03b766cc02fabb1d8ddbf81785e65a73ac9
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-01 15:00:16 +0000

    Tentativa 1, o chat está a perguntar 2 vezes no primeiro ciclo

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string question, response;
+
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            while (question != "EXIT")
+            {
+                question = Console.ReadLine();
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        break;
+                    case "What's your name?":
+                        response = "ChatGPTOne";
+                        break;
+                    case "What's your mission?":
+                        response = "To be better than the ChatGPTZero!";
+                        break;
+                    case "Are you more  intelligent  than me?":
+                        response = "Obviously!";
+                        break;    
+                    default:
+                        response = "I don't recognize your question.";
+                        break;
+                }
+                // Responder adequadamente
+                Console.WriteLine(response);
+                
+            }
+            
+        }
+    }
+}
